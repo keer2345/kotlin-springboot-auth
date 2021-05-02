ef02ea1df4d0e28c72688176fede546bb8f7b0ab
keer2345 <keer2345@gmail.com>
2021-05-02 21:21:01 +0800

    init

---
+++ src/main/kotlin/com/keer/auth/KotlinSpringbootAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.keer.auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringbootAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringbootAuthApplication>(*args)
+}
---
+++ src/test/kotlin/com/keer/auth/KotlinSpringbootAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.keer.auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootAuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
